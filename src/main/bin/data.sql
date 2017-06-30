9755c9c22a1d14cc5b2c5e818b5bd54bb6dcbbc0
e <e>
2017-06-30 12:56:02 -0700

    basic authentication added

---
+++ src/main/bin/data.sql
@@ -0,0 +1,7 @@
+select 1;
+
+INSERT INTO `user` VALUES (1,'deneme','2010-01-01 00:00:00','US','ealparslan@gmail.com',NULL,'Erhan','M','ealparslan','Turkish','Alparslan',NULL,'deneme',NULL,'2017-01-01 00:00:00','CA',NULL,'ealparslan',95117);
+INSERT INTO `user` VALUES (2,'dkdekdj','2010-01-01 00:00:00','US','ouuuz@gmail.com',NULL,'Oguzhan','F','oguzzz','English','Topgul',NULL,'deneme',NULL,'2010-01-01 00:00:00','CA',NULL,'oguzhan',9443);
+INSERT INTO `dieter` VALUES (1,1);
+INSERT INTO `dietician` VALUES (1,3243,'dkfjkdfjk','',3,2);
+INSERT INTO `aggreement` VALUES ('2010-01-01 00:00:00','2010-01-03 00:00:00',121,'month',2,1,1);
